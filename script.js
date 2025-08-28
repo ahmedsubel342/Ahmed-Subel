29bfd758ee94c79f11863f633864b54ca37f65c4
ahmedsubel342 <subelhossen342@gmail.com>
2025-08-28 16:33:49 +0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Simple alert when form is submitted
-document.querySelector("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thank you! Your message has been sent.");
-});
